2815959bdd9a63e94780d1ed0b06df18c83fda2d
serkan <s.safran84@gmail.com>
2021-03-29 14:51:42 +0200

    adding files

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace helloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, What is you name? :");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Hello " + userName + "! How are you?");
+            string userAnswer = Console.ReadLine();
+            userAnswer.ToLower();
+            //Console.WriteLine("I feel " + userAnswer + " as well, " + userName);
+
+            if (userAnswer == "bad")
+            {
+                Console.WriteLine("Don't give up " + userName + " everything will be fine!");
+
+            }
+            else
+            {
+                Console.WriteLine("I feel " + userAnswer + " as well, " + userName);
+            }
+                  
+
+
+
+
+
+
+        }
+    }
+}
